2039be9059e36bc4ab341d6669ac8bb7038a158b
Kairat ALkozhoev <72923175+kkeesha@users.noreply.github.com>
2023-02-23 16:11:35 +0000

    Add files via upload

---
+++ Programs/Personal Accountant/source/model/Common.java
@@ -0,0 +1,18 @@
+package personalaccountant.model;
+
+import personalaccountant.saveload.SaveData;
+
+
+abstract public class Common {
+    
+    public Common() {}
+    
+    public String getValueForComboBox() {
+        return null;
+    }
+    
+    public void postAdd(SaveData sd) {}
+    public void postEdit(SaveData sd) {}
+    public void postRemove(SaveData sd) {}
+    
+}
